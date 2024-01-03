b4784f93396e5609d2c23934337ef6f737fc6b68
Bhagyashreeg15 <bhagyashreeg884@gmail.com>
2024-01-03 20:26:41 +0530

    Initial commit

---
+++ src/main/java/org/Hib_Airline/App.java
@@ -0,0 +1,13 @@
+package org.Hib_Airline;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
